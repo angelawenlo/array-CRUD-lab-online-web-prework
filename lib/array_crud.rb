5bc18f3f8d41250d5e317fd08dd5f03785d0a69b
Angela Lo <angelawenlo@gmail.com>
2019-04-22 17:11:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["dog", "cat", "bird", "turtle"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+ 
 end
 
 def add_element_to_start_of_array(array, element)
